1edc0c8f5dcf64846ef56c0ac0f54c27965532a4
yahfiilham <yahfi.ilham@gmail.com>
2022-04-10 16:07:23 +0700

    update interface CategoryInterface

--- controller/category_controller.go
+++ controller/category_controller.go
@@ -7,9 +7,9 @@ import (
 )
 
 type CategoryController interface {
-	Create(writer http.ResponseWriter, request *http.Request, params httprouter.Param)
-	FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Param)
-	FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Param)
-	Update(writer http.ResponseWriter, request *http.Request, params httprouter.Param)
-	Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Param)
+	Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
 }
